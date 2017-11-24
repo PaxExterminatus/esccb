9520789fcb524a63395fb0d9b3326a78a363a037
voronin <paxexterminatus@gmail.com>
2017-11-24 16:33:59 +0300

    init

---
+++ src/main/kotlin/framework/EntryServlet.kt
@@ -0,0 +1,13 @@
+package framework
+
+import javax.servlet.http.HttpServlet
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@WebServlet(name = "EntryServlet", value = "/*")
+class EntryServlet: HttpServlet() {
+    override fun service(req: HttpServletRequest, resp: HttpServletResponse) {
+        resp.writer.write("Hello!111")
+    }
+}

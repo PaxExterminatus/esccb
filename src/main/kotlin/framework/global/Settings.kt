9f796c6a41d84c9a21902a44af8bf92be5602d4d
voronin <paxexterminatus@gmail.com>
2017-12-26 13:13:47 +0300

    ref

--- src/main/kotlin/framework/global/Settings.kt
+++ src/main/kotlin/framework/global/Settings.kt
@@ -1,15 +1,25 @@
 package framework.global
 
+import java.io.File
+import java.io.InputStream
+
 class Settings {
     val separator = System.getProperty("file.separator")!!
     lateinit var application: String
     lateinit var settings: String
     lateinit var view: String
+    lateinit var app: String
+    lateinit var db: String
+    lateinit var email: String
 
     fun load(ways: String)
     {
         application = ways
         settings = ways + "settings" + separator
         view = ways + "view" + separator
+
+        app = File(settings + "app.json").inputStream().bufferedReader().use {it.readText()}
+        db = File(settings + "db.json").inputStream().bufferedReader().use {it.readText()}
+        email = File(settings + "email.json").inputStream().bufferedReader().use {it.readText()}
     }
 }

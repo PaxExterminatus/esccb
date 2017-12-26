3319b94fb59e464f5c1276bf40012c88a62c00ea
voronin <paxexterminatus@gmail.com>
2017-12-26 13:24:29 +0300

    ref

--- src/main/kotlin/application/implementation/system/StatusModule.kt
+++ src/main/kotlin/application/implementation/system/StatusModule.kt
@@ -16,7 +16,7 @@ class StatusModule: SysModule()
         document.add("<h1>Status Page</h1>")
 
         document.add("<h2>Call</h2>")
-        document.add("Settings: ${Arrays.toString(call.callWays)}")
+        document.add("Ways: ${Arrays.toString(call.callWays)}")
         document.add("Deep: ${call.deep}")
 
         document.add("<h2>Application Settings</h2>")
@@ -25,7 +25,7 @@ class StatusModule: SysModule()
         document.add("Settings: ${settings.settings}")
         document.add("View: ${settings.view}")
 
-        document.add("<h2>Files Settings</h2>")
+        document.add("<h2>Configuration Files</h2>")
         document.add("Application:<br> ${settings.app}")
         document.add("Database:<br> ${settings.db}")
         document.add("Email:<br> ${settings.email}")

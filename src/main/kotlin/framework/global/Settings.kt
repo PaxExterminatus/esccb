0ff531d3cf00113b0a0cc9420bd5886ed1dbaec3
voronin <paxexterminatus@gmail.com>
2017-12-26 14:12:24 +0300

    add json-path

--- src/main/kotlin/framework/global/Settings.kt
+++ src/main/kotlin/framework/global/Settings.kt
@@ -1,13 +1,18 @@
 package framework.global
 
 import java.io.File
+import com.jayway.jsonpath.Configuration
+import com.jayway.jsonpath.JsonPath
 
 class Settings {
     val separator = System.getProperty("file.separator")!!
     lateinit var application: String
     lateinit var settings: String
     lateinit var view: String
+
     lateinit var app: String
+    //lateinit var url: String
+
     lateinit var db: String
     lateinit var email: String
 
@@ -17,8 +22,11 @@ class Settings {
         settings = ways + "settings" + separator
         view = ways + "view" + separator
 
-        app = File(settings + "app.json").inputStream().bufferedReader().use {it.readText()}
-        db = File(settings + "db.json").inputStream().bufferedReader().use {it.readText()}
-        email = File(settings + "email.json").inputStream().bufferedReader().use {it.readText()}
+        app = File(settings + "app.json").inputStream().bufferedReader().use { it.readText() }
+        db = File(settings + "db.json").inputStream().bufferedReader().use { it.readText() }
+        email = File(settings + "email.json").inputStream().bufferedReader().use { it.readText() }
+
+        //val JApp = Configuration.defaultConfiguration().jsonProvider().parse(app)
+        //url = JsonPath.read(JApp,"$.urlHost")
     }
 }

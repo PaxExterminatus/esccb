8986e285600a302a022519959f01c252dca92d55
Paxexterminatus <Paxexterminatus@gmail.com>
2018-01-14 14:09:29 +0300

    ref

--- src/main/kotlin/framework/global/Call.kt
+++ src/main/kotlin/framework/global/Call.kt
@@ -4,48 +4,41 @@ import framework.*
 import java.util.regex.Pattern
 import javax.servlet.http.HttpServletRequest
 
-class Call {
-
-    private var application: String = ""
+class Call
+{
+    lateinit var callWays: Array<String>
+    var deep: Int = 1
+    var application: String = ""
     var module: String = ""
     var work: String = ""
 
-    var deep: Int = 0
-    lateinit var callWays: Array<String>
-
-    fun initCall(req: HttpServletRequest)
-    {
-        val pt = Pattern.compile("/",Pattern.CASE_INSENSITIVE)
-
+    fun initCall(req: HttpServletRequest) {
+        val separatorPattern = Pattern.compile("/",Pattern.CASE_INSENSITIVE)
         val requestURI: String = if (req.requestURI[0] == '/') req.requestURI.substring(1) else req.requestURI
-        callWays = pt.split(requestURI)
+        callWays = separatorPattern.split(requestURI)
         deep = callWays.size
 
-        if (deep > 0)
-        {
-            application = callWays[0]
-            module = "index"
-        }
-        if (deep > 1)
-        {
-            module = callWays[1]
-            work = "index"
-        }
-        if (deep > 2)
-        {
-            work = callWays[2]
-        }
+        application = callWays[0]
 
-        if (settings.debugUse)
-        {
+        module = moduleGet()
+        work = workGet()
+
+        if (settings.debugUse) {
             document.addDebug("application: $application")
             document.addDebug("module: $module")
             document.addDebug("work: $work")
         }
     }
 
-    fun clear()
-    {
+    private fun moduleGet(): String {
+        return if (deep >= 1) callWays[1] else "index"
+    }
+
+    private fun workGet(): String {
+        return if (deep >= 2) callWays[2] else "index"
+    }
+
+    fun clear() {
         application = ""
         module = ""
         work = ""
